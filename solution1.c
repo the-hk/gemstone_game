c22d6a248cd4124abb998f588f5b9ba328d10460
the-hk <conjointchill@gmail.com>
2021-11-21 23:22:01 +0300

    create_child_process functions was completed and it is fully working with pipe structure

--- solution1.c
+++ solution1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
-/////////////*******VARIABLES*************\\\\\\\\\\\\
+#include <wait.h>
+/*******VARIABLES*************/
 
 const int diamond = 3500;
 const int ruby = 50;
 const int sapphire = 1200;
 const int emerald = 800;
+int pid_number;
 
-/////////////*******FUNCTIONS*************\\\\\\\\\\\\
+int fd1[2];  
+int fd2[2];  
+int fd3[2];  
+int fd4[2];  
+int fd5[2]; 
+
+int main_result = 0;
+int sum = 0;
+/*******FUNCTIONS*************/
 
 int go_and_read(int number_of_child){
    char filename[20];
@@ -18,16 +28,16 @@ int go_and_read(int number_of_child){
    while(fscanf(fp1,"%c",&temp)!= EOF){
       
       if(temp == 'd'){
-         printf("diamond \n");
+         //printf("diamond \n");
          result += diamond;
       }else if(temp == 'r'){
-         printf("ruby \n");
+         //printf("ruby \n");
          result += ruby;
       }else if(temp == 's'){
-         printf("sapphire \n");
+         //printf("sapphire \n");
          result += sapphire;
       }else if(temp == 'e'){
-         printf("emerald \n");
+         //printf("emerald \n");
          result += emerald;
       }
    }
@@ -36,9 +46,9 @@ int go_and_read(int number_of_child){
 }
 
 // create 1 mother 5 child process
-int create_process(){
+int create_child_process(){
    int pid = fork();
-   if(pid>0){
+   if(pid>0){      
       int pid2 = fork();
       if(pid2>0){
          int pid3 = fork();
@@ -48,29 +58,68 @@ int create_process(){
                int pid5 = fork();
                if(pid5>0){
                   //parent process
-               }else if(pid4==0){
-                  //child 5
+                  wait(NULL);
+                  read(fd1[0],&main_result,sizeof(int));
+                  close(fd1[0]);
+                  sum += main_result;
+
+                  read(fd2[0],&main_result,sizeof(int));
+                  close(fd2[0]);
+                  sum += main_result;
+
+                  read(fd3[0],&main_result,sizeof(int));
+                  close(fd3[0]);
+                  sum += main_result;
+
+                  read(fd4[0],&main_result,sizeof(int));
+                  close(fd4[0]);
+                  sum += main_result;
+
+                  close(fd5[1]);
+                  read(fd5[0],&main_result,sizeof(int));
+                  sum += main_result;
+                  printf("[PID:%d] total number: %d\n",getpid(),sum);
+               }else if(pid5==0){
+                  //child 4
+                  main_result +=go_and_read(4);
+                  write(fd5[1],&main_result,sizeof(int));
+                  close(fd5[1]);
                }
             }else if(pid4==0){
-               //child 4
+               //child 3
+               main_result +=go_and_read(3);
+               write(fd4[1],&main_result,sizeof(int));
+               close(fd4[1]);
             }
          }else if(pid3==0){
-         //child 3
+         //child 2
+         main_result +=go_and_read(2);
+         write(fd3[1],&main_result,sizeof(int));
+         close(fd3[1]);
          }
       }else if(pid2==0){
-         //child 2
+         //child 1
+         main_result +=go_and_read(1);
+         write(fd2[1],&main_result,sizeof(int));
+         close(fd2[1]);
       }
    }else if(pid==0){
-      //child 2
+      //child 0
+      main_result +=go_and_read(0);
+      close(fd1[0]);
+      write(fd1[1],&main_result,sizeof(int));
+      close(fd1[1]);
    }
-   return 0;
+   return 1;
 }
 
 int main() {
-   // printf() displays the string inside quotation
-   printf("total number = %d\n",go_and_read(0));
-   int a=0;
-   a++;
-   printf("Result 1600\n");
+   pipe(fd1);
+   pipe(fd2);
+   pipe(fd3);
+   pipe(fd4);
+   pipe(fd5);
+
+   create_child_process();
    return 0;
 }
