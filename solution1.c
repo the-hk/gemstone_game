5d2cf490cbd713ac183531fbaf03b5a8573af02b
the-hk <conjointchill@gmail.com>
2021-11-28 01:38:49 +0300

    for solution 1 printing the issues was completed also in solution 2 it tried to create a shared memory between main and child process

--- solution1.c
+++ solution1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <wait.h>
+#include <stdlib.h>
+#include <time.h>
 /*******VARIABLES*************/
 
 const int diamond = 3500;
@@ -8,16 +10,31 @@ const int ruby = 50;
 const int sapphire = 1200;
 const int emerald = 800;
 int pid_number;
+float sleep_amount = 0;
 
-int fd1[2];  
-int fd2[2];  
-int fd3[2];  
-int fd4[2];  
-int fd5[2]; 
+int fd1[2];
+int fd2[2];
+int fd3[2];
+int fd4[2];
+int fd5[2];
 
 int main_result = 0;
 int sum = 0;
 /*******FUNCTIONS*************/
+
+void print_new_child(int main_id,int child_id){
+   printf("[PID:%d] A new child has been created with PID: %d\n",main_id,child_id);
+}
+
+void print_child_sleep(int main_id,int sleep_time){
+   printf("[PID:%d] Tired. Going to sleep for %dmsec.\n",main_id,sleep_time);
+}
+
+int random_sleep(int pid_val_val){
+   srand(pid_val_val); 
+   int r = rand() % 1000;
+   return r;
+}
 
 int go_and_read(int number_of_child){
    char filename[20];
@@ -26,7 +43,7 @@ int go_and_read(int number_of_child){
    sprintf(filename,"buckets/%d.txt",number_of_child);
    FILE *fp1 = fopen(filename,"r");
    while(fscanf(fp1,"%c",&temp)!= EOF){
-      
+
       if(temp == 'd'){
          //printf("diamond \n");
          result += diamond;
@@ -48,7 +65,7 @@ int go_and_read(int number_of_child){
 // create 1 mother 5 child process
 int create_child_process(){
    int pid = fork();
-   if(pid>0){      
+   if(pid>0){
       int pid2 = fork();
       if(pid2>0){
          int pid3 = fork();
@@ -57,8 +74,6 @@ int create_child_process(){
             if(pid4>0){
                int pid5 = fork();
                if(pid5>0){
-                  //parent process
-                  wait(NULL);
                   read(fd1[0],&main_result,sizeof(int));
                   close(fd1[0]);
                   sum += main_result;
@@ -81,34 +96,54 @@ int create_child_process(){
                   printf("[PID:%d] total number: %d\n",getpid(),sum);
                }else if(pid5==0){
                   //child 4
+                  print_new_child(getppid(),getpid());
                   main_result +=go_and_read(4);
                   write(fd5[1],&main_result,sizeof(int));
                   close(fd5[1]);
+                  sleep_amount = random_sleep(getpid());
+                  sleep(sleep_amount/1000);
+                  print_child_sleep(getpid(),sleep_amount);
                }
             }else if(pid4==0){
                //child 3
+               print_new_child(getppid(),getpid());
                main_result +=go_and_read(3);
                write(fd4[1],&main_result,sizeof(int));
                close(fd4[1]);
+               sleep_amount = random_sleep(getpid());
+               sleep(sleep_amount/1000);
+               print_child_sleep(getpid(),sleep_amount);
             }
          }else if(pid3==0){
          //child 2
+         print_new_child(getppid(),getpid());
          main_result +=go_and_read(2);
          write(fd3[1],&main_result,sizeof(int));
          close(fd3[1]);
+         sleep_amount = random_sleep(getpid());
+         sleep(sleep_amount/1000);
+         print_child_sleep(getpid(),sleep_amount);
          }
       }else if(pid2==0){
          //child 1
+         print_new_child(getppid(),getpid());
          main_result +=go_and_read(1);
          write(fd2[1],&main_result,sizeof(int));
          close(fd2[1]);
+         sleep_amount = random_sleep(getpid());
+         sleep(sleep_amount/1000);
+         print_child_sleep(getpid(),sleep_amount);
       }
    }else if(pid==0){
       //child 0
+      print_new_child(getppid(),getpid());
       main_result +=go_and_read(0);
       close(fd1[0]);
       write(fd1[1],&main_result,sizeof(int));
       close(fd1[1]);
+      sleep_amount = random_sleep(getpid());
+      sleep(sleep_amount/1000);
+      print_child_sleep(getpid(),sleep_amount);
    }
    return 1;
 }
--- solution2.c
+++ solution2.c
@@ -1,6 +1,115 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <sys/mman.h>
+#include <string.h>
+
+/*******VARIABLES*************/
+const int diamond = 3500;
+const int ruby = 50;
+const int sapphire = 1200;
+const int emerald = 800;
+
+int fd1[2];
+int fd2[2];
+int fd3[2];
+int fd4[2];
+int fd5[2];
+
+/*******FUNCTIONS*************/
+void *create_shared_memory(size_t size){
+
+  int protection = PROT_READ | PROT_WRITE;
+  int visibility = MAP_SHARED | MAP_ANONYMOUS;
+  return mmap(NULL, size, protection, visibility, -1, 0);
+}
+//--------------------------------------------------
+
+void print_new_child(int main_id,int child_id){
+   printf("[PID:%d] A new child has been created with PID: %d\n",main_id,child_id);
+}
+//--------------------------------------------------
+
+void print_child_sleep(int main_id,int sleep_time){
+   printf("[PID:%d] Tired. Going to sleep for %dmsec.\n",main_id,sleep_time);
+}
+
+
+void go_and_read(int number_of_child,void *shared_mem){
+   char filename[20];
+   char temp;
+   char message;
+   sprintf(filename,"buckets/%d.txt",number_of_child);
+   FILE *fp1 = fopen(filename,"r");
+   while(fscanf(fp1,"%c",&temp)!= EOF){
+      //printf("i cant go out\n");
+      if(temp == 'd'){
+         //printf("diamond \n");
+         message = 'd';
+         memcpy(shared_mem, message, sizeof(message));
+      }else if(temp == 'r'){
+         //printf("ruby \n");
+         message = 'r';
+         memcpy(shared_mem, message, sizeof(message));
+      }else if(temp == 's'){
+         //printf("sapphire \n");
+         message = 's';
+         memcpy(shared_mem, message, sizeof(message));
+      }else if(temp == 'e'){
+         //printf("emerald \n");
+         message = 'e';
+         memcpy(shared_mem, message, sizeof(message));
+      }  
+   }
+}
+
+int creating_child_process(void *shared_mem){
+   int pid=fork();
+
+   if(pid>0){
+      printf("Child read: %s\n", shared_mem);
+      int pid2=fork();
+
+      if(pid2>0){
+         int pid3=fork();
+
+         if(pid3>0){
+            int pid4=fork();
+
+            if(pid4>0){
+               int pid5=fork();
+
+               if(pid5>0){
+               
+               }else if(pid5==0){
+
+               }
+            }else if(pid4==0){
+
+            }
+
+         }else if(pid3==0){
+
+         }
+
+      }else if(pid2==0){
+
+      }
+
+   }else if(pid==0){
+      go_and_read(0,shared_mem);
+   }
+   return 0;
+}
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Result 7250\n");
+   pipe(fd1);
+   pipe(fd2);
+   pipe(fd3);
+   pipe(fd4);
+   pipe(fd5);
+
+   void *shared_mem = create_shared_memory(sizeof(int));
    return 0;
 }
